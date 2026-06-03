970bf6ce5dd7a7fc309f245fb406e0f7ebb2ef8c
juhi <juhigupta200424@gmail.com>
2026-06-03 17:15:08 +0530

    Added registration form UI with validation

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm";
+
+function RegisterPage() {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default RegisterPage;
